a16ed6695205d63d4e9f3d34f5c167a1155ee18e
Kenneth Yuen <Kenneth@Yuen.com>
2021-04-18 12:28:42 -0500

    Part 3 of demo by adding an external node task against a new bpmn - romanEmpire.bpmn

---
+++ src/main/webapp/DecideOnExpansionWorker.js
@@ -0,0 +1,23 @@
+const { Client, logger } = require("camunda-external-task-client-js");
+const { Variables } = require("camunda-external-task-client-js");
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Process Engine
+//  - 'logger': utility to automatically log important events
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+// susbscribe to the topic: 'creditScoreChecker'
+client.subscribe("DecideOnExpansion", async function({ task, taskService }) {
+  // Put your business logic
+  var north = Math.random() >= 0.5;
+
+  // set a process variable 'winning'
+  const processVariables = new Variables();
+  processVariables.set("north", north);
+
+  // complete the task
+  await taskService.complete(task, processVariables);
+});
